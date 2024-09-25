6ce2d5532c7286afbfc6f221c9c3a4bdbdfb4743
Emmanuel Hdez. Maq <emhermaq@gmail.com>
2024-09-24 21:35:40 -0600

    App Heroes con Modulos, Componentes y Rutas Configuradas con lazyLoading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule), //lazyLoading
+  },
+
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule), //lazyLoading
+  },
+
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+
+  // Ruta por defecto 
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
+  },
+
+  // Ruta de error por enlace no conocido
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
