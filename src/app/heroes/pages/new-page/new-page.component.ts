817f4991aeee932c9bde37ca666d03ea9852f4c7
Emmanuel Hdez. Maq <emhermaq@gmail.com>
2024-10-03 21:34:27 -0600

    Se agrego formulario reactivo y ya se inyectaron los servicios en el componente "new-page" para utilizarlos

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Heroe, Publisher } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -6,9 +9,59 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  constructor(
+    private heroesService: HeroesService,
+  ){}
+
+
+  public heroForm = new FormGroup({
+    id:               new FormControl<string>(''),
+    superhero:        new FormControl<string>('', {nonNullable: true} ),
+    publisher:        new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego:        new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters:       new FormControl(''),
+    alt_img:          new FormControl(''),
+  });
+
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ]
+
+
+
+  get currentHero(): Heroe{
+    const hero = this.heroForm.value as Heroe;
+    return hero;
+  }
+
+
+  public onSubmit(): void {
+
+    // console.log({
+    //   formIsValid: this.heroForm.valid,
+    //   value: this.heroForm.value
+    // });
+
+    if( this.heroForm.invalid) return; 
+
+    if( this.currentHero.id ){
+      this.heroesService.updateHero(this.currentHero)
+        .subscribe( hero => {
+          //TODO: mostrar snackbar
+        } );
+
+        return;
+    }
+
+    this.heroesService.addHero( this.currentHero )
+      .subscribe( hero => {
+          //TODO: mostrar snackbar y navegar a /heroes/edit/hero.id
+        }
+      );
+
+
+  }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Heroe } from '../interfaces/heroe.interface';
 import { environments } from '../../../environments/environments';
 
@@ -23,15 +23,38 @@ export class HeroesService {
     }
 
     // Obtener Heroe
-    public getHeroById(id : string): Observable<Heroe | undefined> {
+    public getHeroById( id : string ): Observable<Heroe | undefined> {
         return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`)
         .pipe(
             catchError( error =>  of(undefined) )
         );
     }
     // Buscar un heroe
-    public getSuggestions( query: string): Observable<Heroe[]> {
+    public getSuggestions( query: string ): Observable<Heroe[]> {
         return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
     }
     
+    // Agregar un Heroe
+    public addHero( hero: Heroe ): Observable<Heroe>{
+        return this.http.post<Heroe>(`${this.baseUrl}/heroe`, hero );
+    }
+
+    // Actualizar un Heroe
+    public updateHero( hero: Heroe ): Observable<Heroe>{
+
+        if(!hero.id) throw Error('Hero id is required');
+
+        // return this.http.put<Heroe>(`${this.baseUrl}/heroe/${ hero.id }`, hero );  //Actualiza completamente un obj
+        return this.http.patch<Heroe>(`${this.baseUrl}/heroe/${ hero.id }`, hero );   //Actualiza parcialmente
+    }
+
+    // Borrar un Heroe
+    public deleteHeroById( id: string ): Observable<boolean>{
+
+        return this.http.delete<boolean>(`${this.baseUrl}/heroe/${ id }`)
+            .pipe(
+                catchError( err => of(false) ),
+                map( resp => true ),
+            );
+    }
 }
